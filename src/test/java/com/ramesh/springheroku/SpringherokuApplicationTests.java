11d3e9ab8f73ada65bf0348e6a0030fcb70906c3
Ramesh <rameshkp2@gmail.com>
2021-02-22 00:06:19 +0800

    Created Inital Project in spring boot heroku

---
+++ src/test/java/com/ramesh/springheroku/SpringherokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramesh.springheroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringherokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
