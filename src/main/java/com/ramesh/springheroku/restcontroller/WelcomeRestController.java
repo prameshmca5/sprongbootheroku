e0b874dddc50cdfc7fb8d62a241cfde5826e89bd
Ramesh <rameshkp2@gmail.com>
2021-03-03 02:21:07 +0800

    Added new Container in WelcomeRestController.

---
+++ src/main/java/com/ramesh/springheroku/restcontroller/WelcomeRestController.java
@@ -0,0 +1,17 @@
+package com.ramesh.springheroku.restcontroller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/welcome")
+public class WelcomeRestController {
+
+	@GetMapping
+	public String welcome(){
+		return "Hello docker container this is Welcome Container.";
+	}
+
+
+}
